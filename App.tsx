b8e22240ad9787429ac54db19258e6036fb50818
THasegawa0216 <121989273+THasegawa0216@users.noreply.github.com>
2023-03-08 10:15:07 +0900

    4 files modified, 1 deleted

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import './style.css';
-import './App.css';
 
 export default function App() {
   return (
--- index.tsx
+++ index.tsx
@@ -4,15 +4,40 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
-
+// startログ
 console.log(String('-').repeat(20) + 'Start' + String('-').repeat(20));
 
+const root1Element = document.getElementById('root_1');
+
+const AppGoodBtn = () => {
+  return (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+};
+
+const AppGoodMsg = () => {
+  return <h1>いいね！</h1>;
+};
+
+const AppGoodMsgs = () => {
+  return (
+    <React.Fragment>
+      <h1>h1いいね！</h1>
+      <p>pいいね！</p>
+    </React.Fragment>
+  );
+};
+
+const root = createRoot(root1Element);
+const root2 = createRoot(document.getElementById('root_2'));
+const root3 = createRoot(document.getElementById('root_3'));
+
+// root_1にAppGoodBtnで作成したいいねボタンを配置する(JSX記法)
+root.render(<AppGoodBtn />);
+root2.render(<AppGoodMsg />);
+root3.render(<AppGoodMsgs />);
+
+// endログ
 console.log(String('-').repeat(20) + 'End' + String('-').repeat(20));
